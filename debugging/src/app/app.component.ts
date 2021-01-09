b7b714232ff2d023636cbe8fbd4276e5284c3a61
Sara Jirholm <sara.jirholm@hotmail.com>
2021-01-09 16:18:53 +0100

    Add exercise for debugging

---
+++ debugging/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "The Jungle";
+  plant = {
+    genus: "aroid",
+    species: "Monstera",
+    variant: "Delicioisa"
+  };
+  characteristics = [
+    "Has inflorecens",
+    "Can grow huge like a monster, hence the name",
+    "The older it gets, the more fenestrations it gets"
+  ];
+
+  ngOnInit() {
+    console.log("plant", this.plant);
+  }
+}
