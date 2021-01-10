05cdf01f6b2a706f50a869ca23edc8c6696820e2
Sara Jirholm <sara.jirholm@hotmail.com>
2021-01-10 13:48:28 +0100

    Remove more from state exercise to get it to compile

--- state/src/app/app.module.ts
+++ state/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { SinglePokemonComponent } from "./components/singlePokemon/singlePokemon.component";
-import Favorites from "./components/favorites/favorites.component";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent, SinglePokemonComponent, Favorites],
+  declarations: [AppComponent, SinglePokemonComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
