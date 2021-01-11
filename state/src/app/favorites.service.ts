3383f18aee70188bfa753434a758fb4146a91f27
Sara Jirholm <sara.jirholm@hotmail.com>
2021-01-11 18:56:03 +0100

    Add back more code to make the exercise simpler

---
+++ state/src/app/favorites.service.ts
@@ -0,0 +1,11 @@
+import { BehaviorSubject } from "rxjs";
+import { Injectable } from "@angular/core";
+
+type Favorite = string;
+
+@Injectable({
+  providedIn: "root"
+})
+export class FavoritesService {
+
+}
